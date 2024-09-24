450a9fdfc668c78612abc4473078dd4a00573a80
QafarK <mirqafar2004@gmail.com>
2024-09-24 11:38:52 +0400

    tam deil

--- http/MainWindow.xaml.cs
+++ http/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Text.Json;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 
 
 namespace http;
@@ -13,22 +14,34 @@ public partial class MainWindow : Window
     private List<User> Users;
     private HttpClient _client;
     private int _num = 1;
+    private string _selectedItem;
+
     public MainWindow()
     {
         InitializeComponent();
         Users = [];
         _client = new HttpClient();
+        _selectedItem = string.Empty;
     }
 
     private async void AddOrUpdate(object sender, RoutedEventArgs e) // add | update
     {
         try
         {
-            var user = new User { Id = _num++, Name = name.Text, Surname = surname.Text, Age = Convert.ToInt16(age.Text) };
+            int selectedItemId;
+            User? user = null;
+            if (_selectedItem != string.Empty)
+            {
+                selectedItemId = Convert.ToInt32(_selectedItem.Split(' ')[0].Split(':')[1]);
+                user = new User { Id = selectedItemId, Name = name.Text, Surname = surname.Text, Age = Convert.ToInt16(age.Text) };
+                _selectedItem = string.Empty;
+            }
+            else
+                user = new User { Id = _num++, Name = name.Text, Surname = surname.Text, Age = Convert.ToInt16(age.Text) };
 
             var json = JsonSerializer.Serialize(user);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-
+            // num count !!!!! response
             HttpResponseMessage response = await _client.PostAsync("http://localhost:27001/", content); // Posting to Server
             var responseString = await response.Content.ReadAsStringAsync(); // Reading back Response from Server (Status posting)
 
@@ -37,6 +50,8 @@ public partial class MainWindow : Window
                 MessageBox.Show(DateTime.Now.ToLongTimeString() + " Response from server: " + responseString);
             else
                 MessageBox.Show(DateTime.Now.ToLongTimeString() + " Response from server: NULL");
+
+            GetAllUsers(sender, e);
 
         }
         catch (Exception ex)
@@ -57,6 +72,8 @@ public partial class MainWindow : Window
                 MessageBox.Show("Response from server: " + DateTime.Now.ToLongTimeString());
             else
                 MessageBox.Show(DateTime.Now.ToLongTimeString() + " Response from server: NULL");
+
+
             if (responseString is not null)
             {
                 var splitedResponseString_ = responseString.Split('_');
@@ -65,7 +82,6 @@ public partial class MainWindow : Window
 
                 for (int j = 0; j < splitedResponseString_.Length; j++)
                 {
-                    //MessageBox.Show(splitedResponseString_[j].Split('-')[1].Trim(':')[1].ToString());
 
                     Users.Add(new User
                     {
@@ -86,9 +102,53 @@ public partial class MainWindow : Window
         }
     }
 
-    private void DELETE(object sender, RoutedEventArgs e) // DELETE
+    private async void DELETE(object sender, RoutedEventArgs e) // DELETE
     {
 
+        try
+        {
+            int selectedItemId;
+            if (_selectedItem == string.Empty)
+                MessageBox.Show("Please select Item to Delete");
+            else
+            {
+                selectedItemId = Convert.ToInt32(_selectedItem.Split(' ')[0].Split(':')[1]);
+
+                var content = new StringContent(selectedItemId.ToString(), Encoding.UTF8, "text/plain");
+
+                HttpResponseMessage response = await _client.PutAsync("http://localhost:27001/", content); // Puting to Server
+                var responseString = await response.Content.ReadAsStringAsync(); // Reading back Response from Server (Status puting)
+
+
+                if (responseString is not null)
+                    MessageBox.Show(DateTime.Now.ToLongTimeString() + " Response from server: " + responseString);
+                else
+                    MessageBox.Show(DateTime.Now.ToLongTimeString() + " Response from server: NULL");
+
+                GetAllUsers(sender, e);
+
+                _num = Users.Count + 1;
+                _selectedItem = string.Empty;
+                
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
     }
 
+    private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        try
+        {
+            ListView? _listView = sender as ListView;
+            if (_listView is not null && _listView.SelectedItem is not null)
+                _selectedItem = _listView.SelectedItem.ToString()!;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
 }
--- server/WebHost.cs
+++ server/WebHost.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Linq;
+using System.Net;
 using System.Text.Json;
 namespace server;
 
@@ -38,7 +39,8 @@ public class WebHost
                     _ = Task.Run(() => HandlePostRequest(context));
                 else if (context.Request.HttpMethod == "GET")
                     _ = Task.Run(() => HandleGetRequest(context));
-                //
+                else if (context.Request.HttpMethod == "PUT")
+                    _ = Task.Run(() => HandlePutRequest(context));
             }
             catch (Exception ex)
             {
@@ -47,6 +49,111 @@ public class WebHost
         }
 
     }
+    private async void HandlePostRequest(HttpListenerContext context)
+    {
+        try
+        {
+            // Reading the request body asynchronously
+            using (var reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+            {
+                string requestBody = await reader.ReadToEndAsync();
+                //Console.WriteLine("Request Body: " + requestBody); // Request's full body
+
+                var user = JsonSerializer.Deserialize<User>(requestBody);
+                if (user is not null)
+                {
+                    if (user.Id <= Users.Count) { }
+                    else
+                        user.Id = Users.Count + 1;
+                    Console.WriteLine($"Received User: Id={user.Id}, Name={user.Name}, Surname={user.Surname}, Age={user.Age}");
+                    if (Users.SingleOrDefault((u => u.Id == user.Id)) is not null)
+                    {
+                        int index = Users.FindIndex(u => u.Id == user.Id);
+                        if (index != -1)
+                            Users[index] = user;
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("User index not founed");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+                    else
+                        Users.Add(user);
+                    Console.WriteLine(Users.Count);
+                }
+                else
+                    throw new ArgumentNullException("user is null");
+            }
+            // READING ENDED
+
+            // Respond message to the Client
+            HttpListenerResponse responseToClient = context.Response;
+            responseToClient.ContentType = "text/plain";
+            responseToClient.ContentEncoding = System.Text.Encoding.UTF8;
+
+            string messageToClient = $"Data received successfully\n";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(messageToClient);
+            responseToClient.ContentLength64 = buffer.Length;
+
+            await responseToClient.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+            responseToClient.OutputStream.Close();
+
+            Console.WriteLine("Response message to the Client sent at " + DateTime.Now.ToLongTimeString() + '\n');
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error processing request: {ex.Message}");
+        }
+
+
+    }
+    private async void HandlePutRequest(HttpListenerContext context)
+    {
+        try
+        {
+            using (var reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+            {
+                var requestBody = await reader.ReadToEndAsync();
+                //Console.WriteLine("Request Body: " + requestBody); // Request's full body
+                int id = Convert.ToInt32(requestBody);
+
+                if (id != 0 && id <= Users.Count)
+                {
+
+                    Console.WriteLine($"Received User: Id={id}");
+                    if (Users.SingleOrDefault((u => u.Id == id)) is not null)
+                    {
+                        int index = Users.FindIndex(u => u.Id == id);
+                        if (index != -1)
+                        {
+                            Users.Remove(Users[index]);
+
+                            for (int i = index; i < Users.Count; i++)
+                            {
+                                var a = i;
+                                Users[i].Id = a + 1;
+                            }
+                            reader.Close();
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("User index not founed");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+                }
+                else
+                    throw new ArgumentNullException("user id not founded");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error processing request: {ex.Message}");
+        }
+    }
+
 
     private async void HandleGetRequest(HttpListenerContext context)
     {
@@ -70,51 +177,5 @@ public class WebHost
 
     }
 
-    private async void HandlePostRequest(HttpListenerContext context)
-    {
-        try
-        {
-            // Reading the request body asynchronously
-            using (var reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
-            {
-                string requestBody = await reader.ReadToEndAsync();
-                //Console.WriteLine("Request Body: " + requestBody); // Request's full body
-
-                var user = JsonSerializer.Deserialize<User>(requestBody);
-                if (user is not null)
-                {
-                    if (user.Id == Users.Count) { }
-                    else
-                        user.Id = Users.Count+1;
-                    Console.WriteLine($"Received User: Id={user.Id}, Name={user.Name}, Surname={user.Surname}, Age={user.Age}");
-                    Users.Add(user);
-                    Console.WriteLine(Users.Count);
-                }
-                else
-                    throw new ArgumentNullException("user is null");
-            }
-            // READING ENDED
-
-            // Respond message to the Client
-            HttpListenerResponse responseToClient = context.Response;
-            responseToClient.ContentType = "text/plain";
-            responseToClient.ContentEncoding = System.Text.Encoding.UTF8;
-
-            string messageToClient = $"Data received successfully\nUsers Count:{Users.Count}";
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(messageToClient);
-            responseToClient.ContentLength64 = buffer.Length;
-
-            await responseToClient.OutputStream.WriteAsync(buffer, 0, buffer.Length);
-            responseToClient.OutputStream.Close();
-
-            Console.WriteLine("Response message to the Client sent at " + DateTime.Now.ToLongTimeString() + '\n');
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error processing request: {ex.Message}");
-        }
-
-
-    }
 }
 
